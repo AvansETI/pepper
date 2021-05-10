8893916ce6fe57d918395040a92e78fa28a59b2e
Daphne <daphneboelhouwers@gmail.com>
2021-05-10 08:26:39 +0200

    [Add] angular routing and dark theme

--- zorgmedewerker-angular/src/app/app.module.ts
+++ zorgmedewerker-angular/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { DoctorPageComponent } from './doctor-page/doctor-page.component';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    DoctorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    AppRoutingModule,
+    MatSlideToggleModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
