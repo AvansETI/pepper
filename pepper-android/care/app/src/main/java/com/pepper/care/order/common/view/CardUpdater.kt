a1b7685b9f8589bae44b576166b1128e4589f7ed
Miquel de Leeuw <45978623+deLeeuwMJ@users.noreply.github.com>
2021-06-02 22:00:44 +0200

    [Fix] Cards are now correctly overlapping each other

--- pepper-android/care/app/src/main/java/com/pepper/care/order/common/view/CardUpdater.kt
+++ pepper-android/care/app/src/main/java/com/pepper/care/order/common/view/CardUpdater.kt
@@ -1,47 +1,112 @@
 package com.pepper.care.order.common.view
 
 import android.view.View
-import android.widget.ImageView
-import com.google.android.material.card.MaterialCardView
-import com.pepper.care.R
+import com.ramotion.cardslider.CardSliderLayoutManager
 import com.ramotion.cardslider.DefaultViewUpdater
-import kotlin.math.max
+import kotlin.math.abs
 
 class CardUpdater : DefaultViewUpdater() {
 
+    private lateinit var lm: CardSliderLayoutManager
+    private lateinit var previewView: View
+
+    private var cardWidth = 0
+    private var activeCardLeft = 0
+    private var activeCardRight = 0
+    private var activeCardCenter = 0
+    private var cardsGap = 0f
+
+    private var transitionEnd = 0
+    private var transitionDistance = 0
+    private var transitionRight2Center = 0f
+
+    private var overlapGap = 30f
+
+    override fun onLayoutManagerInitialized(layoutManager: CardSliderLayoutManager) {
+        this.cardWidth = layoutManager.cardWidth
+        this.activeCardLeft = layoutManager.activeCardLeft
+        this.activeCardRight = layoutManager.activeCardRight
+        this.activeCardCenter = layoutManager.activeCardCenter
+        this.cardsGap = layoutManager.cardsGap
+
+        this.transitionEnd = activeCardCenter
+        this.transitionDistance = activeCardRight - transitionEnd
+
+        val centerBorder = (cardWidth - cardWidth * SCALE_CENTER) / 2f
+        val rightBorder = (cardWidth - cardWidth * SCALE_RIGHT) / 2f
+        val right2centerDistance = activeCardRight + centerBorder - (activeCardRight - rightBorder)
+        this.transitionRight2Center = right2centerDistance - cardsGap
+
+        this.lm = layoutManager
+    }
+
     override fun updateView(view: View, position: Float) {
-        super.updateView(view, position)
+        val scale: Float
+        val alpha: Float
+        val z: Float
+        val x: Float
 
-        val card = view.findViewById(R.id.clickable_card) as MaterialCardView
-        val imageView = card.findViewById<ImageView>(R.id.meal_img_src)
-        val alphaView = card.findViewById<View>(R.id.alpha_view)
-
-        if (position < 0) {
-            val alpha = view.alpha
-            view.alpha = 1f
-            alphaView.alpha = 0.9f - alpha
-            imageView.alpha = 0.3f + alpha
-        } else {
-            alphaView.alpha = 0f
-            imageView.alpha = 1f
-        }
-
-        val ratio = layoutManager.getDecoratedLeft(view).toFloat() / layoutManager.activeCardLeft
-        val z: Float = when {
+        when {
             position < 0 -> {
-                Z_CENTER_1 * ratio
+                val ratio = lm.getDecoratedLeft(view).toFloat() / activeCardLeft
+                scale = SCALE_LEFT + SCALE_CENTER_TO_LEFT * ratio
+                alpha = 0.1f + ratio
+                z = Z_CENTER_1 * ratio
+                x = 0f - overlapGap
             }
             position < 0.5f -> {
-                Z_CENTER_1.toFloat()
+                scale = SCALE_CENTER
+                alpha = 1f
+                z = Z_CENTER_1.toFloat()
+                x = 0f
             }
             position < 1f -> {
-                Z_CENTER_2.toFloat()
+                val viewLeft = lm.getDecoratedLeft(view)
+                val ratio =
+                    (viewLeft - activeCardCenter).toFloat() / (activeCardRight - activeCardCenter)
+                scale = SCALE_CENTER - SCALE_CENTER_TO_RIGHT * ratio
+                alpha = 1f
+                z = Z_CENTER_2.toFloat()
+                x =
+                    if (abs(transitionRight2Center) < abs(transitionRight2Center * (viewLeft - transitionEnd) / transitionDistance)) {
+                        -transitionRight2Center
+                    } else {
+                        -transitionRight2Center * (viewLeft - transitionEnd) / transitionDistance
+                    } - overlapGap
             }
             else -> {
-                Z_RIGHT.toFloat()
+                scale = SCALE_RIGHT
+                alpha = 1f
+                z = Z_RIGHT.toFloat()
+
+                val prevViewScale: Float
+                val prevTransition: Float
+                val prevRight: Int
+                val isFirstRight = lm.getDecoratedRight(previewView) <= activeCardRight
+                if (isFirstRight) {
+                    prevViewScale = SCALE_CENTER
+                    prevRight = activeCardRight
+                    prevTransition = 0f
+                } else {
+                    prevViewScale = previewView.scaleX
+                    prevRight = lm.getDecoratedRight(previewView)
+                    prevTransition = previewView.translationX
+                }
+                val prevBorder = (cardWidth - cardWidth * prevViewScale) / 2
+                val currentBorder = (cardWidth - cardWidth * SCALE_RIGHT) / 2
+                val distance =
+                    lm.getDecoratedLeft(view) + currentBorder - (prevRight - prevBorder + prevTransition)
+                val transition = distance - cardsGap
+                x = -transition - overlapGap
             }
         }
 
-        card.cardElevation = max(0f, z)
+        view.scaleX = scale
+        view.scaleY = scale
+        view.z = z
+        view.translationX = x
+        view.alpha = alpha
+
+        previewView = view
     }
 }
