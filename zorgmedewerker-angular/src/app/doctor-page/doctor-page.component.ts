dc5691ad71450ac6af8ebe03f52f6a596ca02cc5
Wout <woutstevens@live.nl>
2021-06-03 16:10:11 +0200

    [Add] Feedback, Answer, Question, Reminder logic angular app

--- zorgmedewerker-angular/src/app/doctor-page/doctor-page.component.ts
+++ zorgmedewerker-angular/src/app/doctor-page/doctor-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from 'src/model/patient';
 import { Allergy } from 'src/model/allergy';
+import { Reminder } from 'src/model/reminder';
+import { Feedback } from 'src/model/feedback';
+import { Answer } from 'src/model/answer';
+import { Question } from 'src/model/question';
 import { MessageHandlerService } from 'src/app/message-handler.service';
 
 @Component({
@@ -11,11 +15,21 @@ import { MessageHandlerService } from 'src/app/message-handler.service';
 export class DoctorPageComponent implements OnInit {
 
   patients: Patient[] = [];
+
   selectedPatient: Patient = null;
-  question: string = ''
+  questions: Question[] = [];
+  answers: Answer[] = [];
+  reminders: Reminder[] = [];
+  feedbacks: Feedback[] = [];
+
+  question: string = '';
 
   constructor(private messageHandler: MessageHandlerService) {
     this.messageHandler.getPatientEmitter().subscribe((patients) => { this.patients = patients });
+    this.messageHandler.getQuestionEmitter().subscribe((questions) => { this.questions = questions });
+    this.messageHandler.getAnswerEmitter().subscribe((answers) => { this.answers = answers });
+    this.messageHandler.getReminderEmitter().subscribe((reminders) => { this.reminders = reminders });
+    this.messageHandler.getFeedbackEmitter().subscribe((feedbacks) => { this.feedbacks = feedbacks });
   }
 
   ngOnInit(): void {
@@ -32,6 +46,27 @@ export class DoctorPageComponent implements OnInit {
 
   formatAllergies(allergies: Set<Allergy>): string {
     return Array.from(allergies).join(', ').toLowerCase()
+  }
+
+  formatTimestamp(d: Date): string {
+    if (d === null) {
+      return 'ERROR';
+    }
+    return ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+  }
+
+  formatDate(d: Date): string {
+    if (d === null) {
+      return 'ERROR';
+    }
+    return ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + d.getFullYear();
+  }
+
+  onTest(patientId: string): void {
+    this.messageHandler.requestQuestions(patientId);
+    this.messageHandler.requestAnswers(patientId);
+    this.messageHandler.requestReminders(patientId);
+    this.messageHandler.requestFeedbacks(patientId);
   }
 
   onSend(patientId: string): void {
